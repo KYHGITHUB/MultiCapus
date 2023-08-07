3711ecc8806423202918627085a7c4614c504a2d
rnjsdydgus0 <rnjsdydgus0@gmail.com>
2023-08-07 17:57:12 +0900

    230807 멀티캠퍼스 강의 커밋 웹 크롤링 공부. bs(urlopen, 'html.parser') 바로 안먹힐떈 User-Agent 사용하기 크롤링 후에 필요한 데이터만 모아서 데이터 프레임 만들기

--- client.py
+++ client.py
@@ -1,101 +1,124 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 13 15:22:47 2023
-
-@author: rnjsd
-"""
-
-import python_module as pm
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import os
 import re
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+pd.set_option('display.max_columns', None)      # 데이터프레임 끝까지 보여주기
+pd.set_option('display.max_rows', None)
+
 
 '''
-#%%230804
+with open('test.html', encoding = 'utf=8') as f:
+    line = f.read()
+line
+soup = bs(line, 'html.parser')
+print(soup)
+print(soup.prettify())
+print(soup.children)
+soup_children_list = list(soup.children)
+list(soup.children)[1]
+print(soup_children_list)
+print(soup.body)
+print(soup.head)
+print(soup.find_all('p'))
+for i in soup.find_all('p'):
+    print(i.text)
 
-path = 'D:\.spyder-py3\class file\example_data\example_data'
-file_list = os.listdir(path)
-print(file_list)
-df = pd.read_csv('D:\.spyder-py3\class file\example_data\example_data\ex5.csv')
-print(df)
-df = pd.read_csv(path+'\ex5.csv', index_col = 0)
-print(df)
-df = pd.read_csv(path+'\ex5.csv', index_col = [0])
-print(df)
-print(pd.isnull(df))
-print(df.info())
-print(df.describe())
+news = ' https://news.daum.net'
+soup = bs(urlopen(news), 'html.parser')
+print(soup)
+print(soup.find_all('div', {'class':'item_issue'}))    # div 태그에서 class(key)값이 item_issue(value)인것을 찾아라
+for i in soup.find_all('div', {'class':'item_issue'}):
+    print(i.text)
+print(soup.find_all('a')[:5])
+for i in soup.find_all('a'):
+    print(i.get('href'))
+for i in soup.find_all('div', {'class':'item_issue'}):
+    print(i.find_all('a')[0].get('href'))
+    
+article1 = 'https://go.seoul.co.kr/news/newsView.php?id=20200427004004&wlog_tag3=daum'
+soup = bs(urlopen(article1).read(), 'html.parser')
+print(soup)
+for i in soup.find_all('p'):
+    print(i.text)
+    
+news = ' https://news.daum.net'
+soup = bs(urlopen(news), 'html.parser')
+headline = soup.find_all('div', {'class':'item_issue'})
+for i in headline:
+    print(i)
+for i in headline:
+    print(i.text)
+    soup3 = bs(urlopen(i.find_all('a')[0].get('href')).read(), 'html.parser')
+    for j in soup3.find_all('p'):
+        print(j.text)
+for i in soup.find_all('div', {'class':'item_issue'}):
+    print(i.find_all('a')[0].get('href'))
+with open('link.txt', 'w') as f:
+    for i in soup.find_all('div', {'class':'item_issue'}):
+        f.write(i.find_all('a')[0].get('href')+'\n')
 
-df.to_csv('230804테스트용.txt', index=False)
-print(os.listdir())
-df.to_csv('230804테스트_header=False.txt', index=False, header=False)
-print(os.listdir())
-df.to_csv('확인용.txt', sep ='|')
-print(os.listdir())
+article1 = 'https://v.daum.net/v/20230807102700905'
+soup = bs(urlopen(article1).read(), 'html.parser')
+with open('article1.txt', 'w') as f:
+    for i in soup.find_all('p'):        # p태그는 기사내용을 찾기위함이다
+        f.write(i.text+'\n')
 
-result_yes = re.match(r'life', 'life is good')
-print(result_yes)
+url = 'https://news.daum.net/'
+soup = bs(urlopen(url).read(), 'html.parser')
+headline = soup.find_all('div', {'class':'item_issue'})
+headline
 
-print(re.search(r'so', 'Life is so good. so wonderful'))
+for i in headline:
+    print(i.text)
+for i in headline:
+    print(i.text)
+    print(i.find_all('a')[0].get('href'))
+    new_url = i.find_all('a')[0].get('href')
+    soup2 = bs(urlopen(new_url).read(), 'html.parser')
+    for j in soup2.find_all('p'):
+        print(j.text+'\n')
 
-number = 'My number is 511223-1****** and your is 521012-2******, 598278'
-print(re.findall('\d{6}', number))
-
-example = '이동민 교수는 다음과 같이 설명했습니다.(이동민, 2019). 그런데 다른 학자는 이 문제에 대해서 다른 견해를 가지고 있었습니다(최재영, 2019). 또 다른 견해도 있었습니다(Lion, 2018).'
-print(re.findall(r'\(.+?\)', example))
-sentence = 'I have a lovely dog, really. I am not telling a lie. What a pretty dog! I love this dog.'
-print(re.sub(r'dog', 'cat', sentence))
-words = 'I am home now. \n\n\nI am with my cat.\n\n'
-print(words)
-print(re.sub('\n', '', words))
+with open('article_total.txt', 'w',encoding = 'utf-8') as f:
+    for i in headline:
+        f.write(i.text)
+        f.write(i.find_all('a')[0].get('href')+'\n')
+        new_url = i.find_all('a')[0].get('href')
+        soup2 = bs(urlopen(new_url).read(), 'html.parser')
+        for j in soup2.find_all('p'):
+            f.write(j.text+'\n')
 '''
-with open('friends101.txt') as f:
-    script101 = f.read()
-print(script101)
-line = re.findall(r'Monica:.+', script101)
-print(line)
-with open('friends101_monica.txt', 'w') as f:
-    f.write('\n'.join(line))
-    
-char = re.compile(r'[A-Z][a-z]+:')
-name_script101 = re.findall(char, script101)
-print(list(set(name_script101)))
-with open('chracters.txt', 'w') as f:
-    f.write('\n'.join(list(set(name_script101))))
-
-character = [x[:-1] for x in list(set(re.findall(r'[A-Z][a-z]+:', script101)))]
-print(character)
-re.findall(r'\([A-Za-z].+?[a-z|\.]\)', script101)[:6]
-sentence = '(가asknasdk.), (qwㄴㅁ어ㅁㄴ우ㅏㅁㄴ.), (qwejnaskldxzkcnasd,asdasqwkqwnkeqwelsadkasd)'
-print(re.findall(r'\([A-Za-z].+?[a-z|\.]\)', sentence))
-
-with open('friends101.txt') as f:
-    sentence = f.readlines()
-    
-lines = []
-for i in sentence:
-    if re.match(r'[A-Za-z]+:', i):
-        lines.append(i)
-print(lines[:4])
-
-would_list = []
-for i in lines:
-    if re.search('would', i):
-        would_list.append(i)
-print(would_list)
-print(re.match('[0-9]', '567').group())     # [0-9] == \d
-print(re.match('[1-4]', '45367').group())
-print(re.match('5[1-4]', '51267').group())
-print(re.match('[0-9]+', '512367').group())
-print(re.search('는.+[\d]', '나는 낭만 고1양이').group())
-print(re.search('는.+[\d]', '나는 낭만 고1양이').span())
-print(re.findall(r'[a-z]+', 'python 3 version program'))
-p = re.compile(r'([A-Za-z]\w*)\s*=\s*(\d+)')
-print(p.search('a = 123').group())
-re.match(r'a.*b', 'acbd, asefsdgweryerb').group()
-re.match(r'\s\w*', ' abcdefg').group()
-
-
+url = 'https://www.chicagomag.com/chicago-magazine/january-2023/our-30-favorite-things-to-eat-right-now/'
+hdr = {'User-Agent':'Mozilla/5.0'}
+req = Request(url, headers=hdr)
+page = urlopen(req)
+soup = bs(page, 'html.parser')
+soup
+temp = soup.find_all('div', {'class':'article-body'})[0]
+temp
+f_list = []
+r_list = []
+p_list = []
+a_list = []
+temp.find_all('h2')[0].text
+for f in temp.find_all('h2'):
+    f_list.append(f.text)
+f_list
+temp.find_all('h3')[22].text.split('at')[1]
+for r in temp.find_all('h3'):
+    r_list.append(r.text.split('at')[1].strip())
+r_list
+temp.find_all('p')[0].text.index('$')
+temp.find_all('p')[0].text[314]
+temp.find_all('p')[0].text[314:].split()[0].strip('.')
+' '.join(temp.find_all('p')[0].text[314:].split()[1:]).strip()
+for p in temp.find_all('p'):
+    p_index = p.text.index('$')
+    p_list.append(p.text[p_index:].split()[0].strip('.'))
+    a_list.append(' '.join(p.text[p_index:].split()[1:]).strip())
+p_list
+a_list
+data = {'Food':f_list, 'Restaurant':r_list, 'Price':p_list, 'Address':a_list}
+data
+df = pd.DataFrame(data)
+print(df)
 
--- origin_client.py
+++ origin_client.py
@@ -26,6 +26,7 @@ import seaborn as sns
 #   %matplotlib -> 로컬에선 쓰지않고 코랩에선 라인에 그래프가 포함되게 해달라는
 #                  명령어.
 import copy
+import re
 #%% 230713
 
 pm.test()
@@ -1159,3 +1160,87 @@ for i in df.columns:
 path = 'D:\\.spyder-py3\\class file\\example_data\\example_data\\'
 df = pd.read_csv(path + 'ex1.csv', sep = ',')
 print(df)
+#%%230804
+
+path = 'D:\.spyder-py3\class file\example_data\example_data'
+file_list = os.listdir(path)
+print(file_list)
+df = pd.read_csv('D:\.spyder-py3\class file\example_data\example_data\ex5.csv')
+print(df)
+df = pd.read_csv(path+'\ex5.csv', index_col = 0)
+print(df)
+df = pd.read_csv(path+'\ex5.csv', index_col = [0])
+print(df)
+print(pd.isnull(df))
+print(df.info())
+print(df.describe())
+
+df.to_csv('230804테스트용.txt', index=False)
+print(os.listdir())
+df.to_csv('230804테스트_header=False.txt', index=False, header=False)
+print(os.listdir())
+df.to_csv('확인용.txt', sep ='|')
+print(os.listdir())
+
+result_yes = re.match(r'life', 'life is good')
+print(result_yes)
+
+print(re.search(r'so', 'Life is so good. so wonderful'))
+
+number = 'My number is 511223-1****** and your is 521012-2******, 598278'
+print(re.findall('\d{6}', number))
+
+example = '이동민 교수는 다음과 같이 설명했습니다.(이동민, 2019). 그런데 다른 학자는 이 문제에 대해서 다른 견해를 가지고 있었습니다(최재영, 2019). 또 다른 견해도 있었습니다(Lion, 2018).'
+print(re.findall(r'\(.+?\)', example))
+sentence = 'I have a lovely dog, really. I am not telling a lie. What a pretty dog! I love this dog.'
+print(re.sub(r'dog', 'cat', sentence))
+words = 'I am home now. \n\n\nI am with my cat.\n\n'
+print(words)
+print(re.sub('\n', '', words))
+
+with open('friends101.txt') as f:
+    script101 = f.read()
+print(script101)
+line = re.findall(r'Monica:.+', script101)
+print(line)
+with open('friends101_monica.txt', 'w') as f:
+    f.write('\n'.join(line))
+    
+char = re.compile(r'[A-Z][a-z]+:')
+name_script101 = re.findall(char, script101)
+print(list(set(name_script101)))
+with open('chracters.txt', 'w') as f:
+    f.write('\n'.join(list(set(name_script101))))
+
+character = [x[:-1] for x in list(set(re.findall(r'[A-Z][a-z]+:', script101)))]
+print(character)
+re.findall(r'\([A-Za-z].+?[a-z|\.]\)', script101)[:6]
+sentence = '(가asknasdk.), (qwㄴㅁ어ㅁㄴ우ㅏㅁㄴ.), (qwejnaskldxzkcnasd,asdasqwkqwnkeqwelsadkasd)'
+print(re.findall(r'\([A-Za-z].+?[a-z|\.]\)', sentence))
+
+with open('friends101.txt') as f:
+    sentence = f.readlines()
+    
+lines = []
+for i in sentence:
+    if re.match(r'[A-Za-z]+:', i):
+        lines.append(i)
+print(lines[:4])
+
+would_list = []
+for i in lines:
+    if re.search('would', i):
+        would_list.append(i)
+print(would_list)
+print(re.match('[0-9]', '567').group())     # [0-9] == \d
+print(re.match('[1-4]', '45367').group())
+print(re.match('5[1-4]', '51267').group())
+print(re.match('[0-9]+', '512367').group())
+print(re.search('는.+[\d]', '나는 낭만 고1양이').group())
+print(re.search('는.+[\d]', '나는 낭만 고1양이').span())
+print(re.findall(r'[a-z]+', 'python 3 version program'))
+p = re.compile(r'([A-Za-z]\w*)\s*=\s*(\d+)')
+print(p.search('a = 123').group())
+re.match(r'a.*b', 'acbd, asefsdgweryerb').group()
+re.match(r'\s\w*', ' abcdefg').group()
+
